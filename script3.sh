6ce5426c82f9930a73bd3f0717ed897762a1f3d6
Даниил <160046659+deadxraver@users.noreply.github.com>
2024-05-20 12:43:51 +0300

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,22 @@
+ln -s lab0/pikachu8 lab0/Copy_29
+cp lab0/cottonee9 lab0/bastiodon4/samurottcottonee
+chmod u=rwx lab0/boldore0
+ln -P lab0/boldore0 lab0/pikachu8/manectricboldore
+chmod u=r lab0/boldore0
+chmod u=rwx lab0/steelix3 lab0/pikachu8/piloswine
+chmod u=rwx lab0/steelix3/skorupi
+cp -r lab0/steelix3 lab0/pikachu8/piloswine
+chmod u-rwx lab0/steelix3/skorupi
+chmod u=rx lab0/steelix3
+chmod u=wx lab0/pikachu8/piloswine
+chmod u=rwx lab0/remoraid5
+ln -s lab0/remoraid5 lab0/pikachu8/hitmontopremoraid
+chmod u-rwx lab0/remoraid5
+chmod u=rwx lab0/cottonee9 lab0/bastiodon4/blitzle
+cp lab0/cottonee9 lab0/bastiodon4/blitzle/cottonee9
+chmod u=r lab0/cottonee9
+chmod 571 lab0/bastiodon4/blitzle
+chmod u=rwx lab0/bastiodon4/barboach lab0/bastiodon4/samurott
+cat lab0/bastiodon4/barboach lab0/bastiodon4/samurott > lab0/boldore0_87
+chmod 400 lab0/bastiodon4/barboach
+chmod 044 lab0/bastiodon4/samurott
