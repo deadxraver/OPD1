fd823ee491c19b041242160b47ade0ba5d940a88
Даниил <160046659+deadxraver@users.noreply.github.com>
2024-05-20 12:42:28 +0300

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,28 @@
+cd lab0
+cd bastiodon4
+chmod 571 blitzle
+chmod 400 barboach
+chmod 004 samurott
+cd ..
+chmod a=rwx,g-r bastiodon4
+chmod 000 boldore0
+chmod uo=r boldore0
+chmod 000 cottonee9
+chmod uo=r cottonee9
+cd pikachu8
+chmod 524 dugtrio
+chmod 764 lumineon
+chmod 440 hitmontop
+chmod a=wx,o=rx piloswine
+chmod 000 manectric
+chmod uo=r manectric
+cd ..
+chmod a=r,ug+w,u+x pikachu8
+chmod 004 remoraid5
+cd steelix3
+chmod 570 prinplup
+chmod a-rwx,o+r skorupi
+chmod 513 porygon
+cd ..
+chmod 512 steelix3
+cd ..
