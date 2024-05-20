54acf99b4f84a591002250e912e3629ac2fa635d
Даниил <160046659+deadxraver@users.noreply.github.com>
2024-05-20 12:44:36 +0300

    Create script5.sh

---
+++ script5.sh
@@ -0,0 +1,11 @@
+chmod u+w lab0/remoraid5
+rm lab0/remoraid5
+chmod u+w lab0/pikachu8/manectric
+rm lab0/pikachu8/manectric
+rm lab0/pikachu8/hitmontopremora*
+chmod u+w lab0/pikachu8/manectricboldo*
+rm lab0/pikachu8/manectricboldo*
+chmod u+w lab0/bastiodon4/blitzle
+chmod u+w lab0/bastiodon4/blitzle/cottonee9
+rm -rf lab0/bastiodon4
+rmdir lab0/pikachu8/dugtrio
