1002a0aa3e2fc17f918d42f18b5e43df6d77dc86
Даниил <160046659+deadxraver@users.noreply.github.com>
2024-05-20 12:41:44 +0300

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,31 @@
+chmod -R 777 lab0
+rm -rf lab0
+mkdir lab0
+cd lab0
+mkdir bastiodon4
+cd bastiodon4
+mkdir blitzle
+echo -e "Возможности Overland=3 Surface=8 Underwater=6 Jump=3\nPower=1
+Intelligence=3 Gilled=0" > barboach
+echo -e "Ходы Aqua Tail Block Covet\nHelping Hand Icy Wind Iron Tail Knock Off
+Megahorn!= Sleep Talk Snore\nSuperpower Water Pledge" > samurott
+cd ..
+echo -e "satk=5 sdef=4\nspd=2" > boldore0
+echo -e "Возможности Overland=5 Sky=6 Surface=3 Jump=5\nPower1=0 Intelligence=4
+Sprouter=0 Shrinkable=0" > cottonee9
+mkdir pikachu8
+cd pikachu8
+mkdir dugtrio
+mkdir lumineon
+echo -e "Развитые\nспособности Steadfast" > hitmontop
+mkdir piloswine
+echo "Тип диеты Carnivore" > manectric
+cd ..
+echo -e "Тип\nпокемона WATER NONE" > remoraid5
+mkdir steelix3
+cd steelix3
+mkdir prinplup
+echo "Тип покемона POISON BUG" > skorupi
+mkdir porygon
+cd ..
+cd ..
